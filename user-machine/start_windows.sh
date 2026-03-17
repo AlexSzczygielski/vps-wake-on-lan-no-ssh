4e84d85580000fbb3ee5f279e13c3347900bcf75
AlexSzczygielski <a.szczygielski21@gmail.com>
2026-03-17 12:26:20 +0000

    Added bash command for client computer to automatically call WOL form shell

---
+++ user-machine/start_windows.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Automation script - call wakeonlan directly from your computer. 
+#Replace TOKEN and port with your data
+
+TOKEN=""
+PORT=""
+
+# Colors
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[0;34m"
+CYAN="\033[0;36m"
+MAGENTA="\033[0;35m"
+RESET="\033[0m"   # resets color
+
+
+echo -e "${CYAN} ▗▄▄▖▗▄▄▄▖▗▄▖ ▗▄▄▖▗▄▄▄▖    ▗▖ ▗▖▗▄▄▄▖▗▖  ▗▖▗▄▄▄  ▗▄▖ ▗▖ ▗▖ ▗▄▄▖
+▐▌     █ ▐▌ ▐▌▐▌ ▐▌ █      ▐▌ ▐▌  █  ▐▛▚▖▐▌▐▌  █▐▌ ▐▌▐▌ ▐▌▐▌   
+ ▝▀▚▖  █ ▐▛▀▜▌▐▛▀▚▖ █      ▐▌ ▐▌  █  ▐▌ ▝▜▌▐▌  █▐▌ ▐▌▐▌ ▐▌ ▝▀▚▖
+▗▄▄▞▘  █ ▐▌ ▐▌▐▌ ▐▌ █      ▐▙█▟▌▗▄█▄▖▐▌  ▐▌▐▙▄▄▀▝▚▄▞▘▐▙█▟▌▗▄▄▞▘
+                                                               
+                                                               
+                                                               ${RESET}"
+
+echo "Sending request with TOKEN: $TOKEN"
+
+# Get the response
+RESPONSE=$(curl -s "http://frog02.mikr.us:$PORT/wol_request?token=$TOKEN")
+
+# Check response and print in color
+if [[ "$RESPONSE" == "Request accepted" ]]; then
+    echo -e "${GREEN}$RESPONSE${RESET}"
+else
+    echo -e "${RED}$RESPONSE${RESET}"
+fi
